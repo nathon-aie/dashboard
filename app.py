252e2c29f5ddc7defc9485cf208f486c6b613b30
Polkrit Bualoy <pkblnthn@gmail.com>
2026-03-01 11:23:30 +0700

    Make a simple box plot

--- app.py
+++ app.py
@@ -1,132 +1,3 @@
-# from dash import Dash, html, dcc, Input, Output  # เพิ่ม Input, Output
-# from pandas.api.types import CategoricalDtype
-# import dash_ag_grid as dag
-# import pandas as pd
-# import plotly.express as px
-
-# # 1. เตรียมข้อมูล
-# df = pd.read_csv("Dataset/Smartphone_Usage_Productivity_Dataset_50000.csv")
-
-# bins = [0, 20, 30, 40, 50, 100]
-# labels = ["<20", "21-30", "31-40", "41-50", ">50"]
-# df["Age_Group"] = pd.cut(df["Age"], bins=bins, labels=labels)
-# cat_type = CategoricalDtype(categories=labels, ordered=True)
-# df["Age_Group"] = df["Age_Group"].astype(cat_type)
-
-# x_options = [
-#     {"label": "Age Group", "value": "Age_Group"},
-#     {"label": "Gender", "value": "Gender"},
-#     {"label": "Occupation", "value": "Occupation"},
-# ]
-
-# y_options = [
-#     {"label": "Daily Phone Hours", "value": "Daily_Phone_Hours"},
-#     {"label": "Social Media Hours", "value": "Social_Media_Hours"},
-#     {"label": "Productivity Score", "value": "Work_Productivity_Score"},
-#     {"label": "Sleep Hours", "value": "Sleep_Hours"},
-#     {"label": "Stress Level", "value": "Stress_Level"},
-# ]
-
-# app = Dash(__name__)
-
-# app.layout = html.Div(
-#     [
-#         html.H1("Smartphone Usage Dashboard", style={"textAlign": "center"}),
-#         # ส่วนของ Dropdown สำหรับเลือกข้อมูล
-#         html.Div(
-#             [
-#                 html.Div(
-#                     [
-#                         html.Label("เลือกกลุ่มข้อมูล (แกน X):"),
-#                         dcc.Dropdown(
-#                             id="x-axis-select", options=x_options, value="Age_Group"
-#                         ),
-#                     ],
-#                     style={"marginBottom": "10px"},
-#                 ),  # ใส่ Margin ล่างเพื่อให้มีระยะห่าง
-#                 html.Div(
-#                     [
-#                         html.Label("เลือกสถิติที่สนใจ (แกน Y):"),
-#                         dcc.Dropdown(
-#                             id="y-axis-select",
-#                             options=y_options,
-#                             value="Daily_Phone_Hours",
-#                         ),
-#                     ]
-#                 ),
-#             ],
-#             style={"width": "30%", "margin": "20px auto", "textAlign": "left"},
-#         ),
-#         # ส่วนแสดงกราฟ
-#         html.Div(
-#             [
-#                 # กราฟซ้าย (กราฟ Dynamic ที่เลือกจาก Dropdown)
-#                 html.Div(
-#                     [dcc.Graph(id="dynamic-graph")],
-#                     style={"width": "50%", "display": "inline-block"},
-#                 ),
-#                 # กราฟขวา (กราฟคงที่ หรือกราฟอื่นๆ)
-#                 html.Div(
-#                     [
-#                         dcc.Graph(
-#                             id="dynamic-pie-chart",
-#                             figure=px.pie(
-#                                 df, names="Device_Type", title="Device Distribution"
-#                             ),
-#                         )
-#                     ],
-#                     style={"width": "50%", "display": "inline-block"},
-#                 ),
-#             ],
-#             style={"display": "flex"},
-#         ),  # ใช้ Flex เพื่อให้ Div ลูกสองอันวางขนานกัน
-#         # ตารางข้อมูล
-#         dag.AgGrid(
-#             rowData=df.to_dict("records"),
-#             columnDefs=[{"field": i} for i in df.columns],
-#         ),
-#     ]
-# )
-
-
-# # 3. สร้าง Callback เพื่ออัปเดตกราฟ
-# @app.callback(
-#     Output("dynamic-graph", "figure"),
-#     Output("dynamic-pie-chart", "figure"),
-#     Input("x-axis-select", "value"),
-#     Input("y-axis-select", "value"),
-# )
-# def update_graph(x_col, y_col):
-#     # กำหนดเงื่อนไขการเรียงลำดับ (เฉพาะ Age_Group)
-#     order = {"Age_Group": labels} if x_col == "Age_Group" else None
-
-#     # สร้างกราฟ Histogram (ค่าเฉลี่ย)
-#     fig = px.histogram(
-#         df,
-#         x=x_col,
-#         y=y_col,
-#         histfunc="avg",
-#         category_orders=order,
-#         title=f"Average {y_col.replace('_', ' ')} by {x_col.replace('_', ' ')}",
-#         color=x_col,
-#         barmode="group",
-#     )
-#     fig.update_layout(yaxis_title="Average Value", transition_duration=500)
-
-#     pie_fig = (
-#         px.pie(
-#             df,
-#             names=x_col,
-#             title=f"Distribution of {x_col.replace('_', ' ')}",
-#         ),
-#     )
-#     pie_fig.update_layout(transition_duration=500)
-
-#     return fig, pie_fig
-
-
-# if __name__ == "__main__":
-#     app.run(debug=True)
 from dash import Dash, html, dcc, Input, Output
 from pandas.api.types import CategoricalDtype
 import dash_ag_grid as dag
@@ -232,6 +103,11 @@ app.layout = html.Div(
                 ),
             ],
             style={"display": "flex", "padding": "0 20px"},
+        ),
+        dcc.Graph(
+            figure=px.box(
+                df, x="Occupation", y="Sleep_Hours", title="Phone Usage by Occupation"
+            )
         ),
         # ตารางข้อมูลด้านล่าง
         html.Div(
