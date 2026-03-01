9c53c07b8a22b3017f9be59b59e14a20721e380e
Polkrit Bualoy <pkblnthn@gmail.com>
2026-03-01 10:05:43 +0700

    Update Dashboard Title

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # Import packages
-from dash import Dash, html, dcc
+from dash import Dash, html
 import dash_ag_grid as dag
 import pandas as pd
 import plotly.express as px
@@ -7,16 +7,27 @@ import plotly.express as px
 # Incorporate data
 df = pd.read_csv("Dataset/Smartphone_Usage_Productivity_Dataset_50000.csv")
 
+bins = [0, 20, 30, 40, 50, 100]
+labels = ["<20", "21-30", "31-40", "41-50", ">50"]
+df["Age_Group"] = pd.cut(df["Age"], bins=bins, labels=labels)
+
+occupations = ["All"] + list(df["Occupation"].unique())
+devices = ["All"] + list(df["Device_Type"].unique())
+
 # Initialize the app
 app = Dash()
+app.title = "Smartphone Usage and Productivity Dashboard"
 
 # App layout
 app.layout = [
-    html.Div(children="My First App with Data"),
-    dag.AgGrid(
-        rowData=df.to_dict("records"), columnDefs=[{"field": i} for i in df.columns]
+    html.Div(
+        children="Smartphone Usage and Productivity Dashboard",
+        style={"textAlign": "center", "fontSize": 24},
     ),
-    # dcc.Graph(figure=px.histogram(df, x="Age_Group", y="Productivity_Score", histfunc="avg")),
+    dag.AgGrid(
+        rowData=df.to_dict("records"),
+        columnDefs=[{"field": i} for i in df.columns],
+    ),
 ]
 
 # Run the app
