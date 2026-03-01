01119cb1c39a4b3172ebaab433020e0a1ecb0fae
Polkrit Bualoy <pkblnthn@gmail.com>
2026-03-01 10:21:49 +0700

    Try using histogram with age group and daily phone hours

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 # Import packages
-from dash import Dash, html
+from dash import Dash, html, dcc
+from pandas.api.types import CategoricalDtype
 import dash_ag_grid as dag
 import pandas as pd
 import plotly.express as px
@@ -10,6 +11,8 @@ df = pd.read_csv("Dataset/Smartphone_Usage_Productivity_Dataset_50000.csv")
 bins = [0, 20, 30, 40, 50, 100]
 labels = ["<20", "21-30", "31-40", "41-50", ">50"]
 df["Age_Group"] = pd.cut(df["Age"], bins=bins, labels=labels)
+cat_type = CategoricalDtype(categories=labels, ordered=True)
+df["Age_Group"] = df["Age_Group"].astype(cat_type)
 
 occupations = ["All"] + list(df["Occupation"].unique())
 devices = ["All"] + list(df["Device_Type"].unique())
@@ -27,6 +30,16 @@ app.layout = [
     dag.AgGrid(
         rowData=df.to_dict("records"),
         columnDefs=[{"field": i} for i in df.columns],
+    ),
+    dcc.Graph(
+        figure=px.histogram(
+            df,
+            x="Age_Group",
+            y="Daily_Phone_Hours",
+            histfunc="avg",
+            category_orders={"Age_Group": labels},
+            title="Average Daily Phone Hours by Age Group",
+        )
     ),
 ]
 
